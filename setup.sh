11f2fc2fc660f2faa80786ed4f1ccab0796314f4
JUNG GAYEON <113704015+Gayeon6423@users.noreply.github.com>
2025-06-10 20:16:18 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -24,6 +24,7 @@ fi
 # Install package
 echo "🔹 Installing dependencies..."
 pip install --upgrade pip
+pip install torch==2.4.1 torchvision==0.19.1 --index-url https://download.pytorch.org/whl/cu121
 pip install -r requirements.txt
 
 echo "✅ Setup complete!"
